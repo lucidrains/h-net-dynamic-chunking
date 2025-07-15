57034302d96c562025268d242d20bfad9d5c76b6
lucidrains <lucidrains@gmail.com>
2025-07-15 09:26:51 -0700

    throw e2e into readme

--- tests/test_chunker.py
+++ tests/test_chunker.py
@@ -1,4 +1,13 @@
+import torch
 import pytest
 
 def test_chunker():
-    assert True
+    from h_net_dynamic_chunking.h_net_dynamic_chunking import DynamicChunkingDownsampler
+
+    downsampler = DynamicChunkingDownsampler(512)
+
+    tokens = torch.randn(3, 1024, 512).requires_grad_()
+
+    downsampled, upsample_fn, *_ = downsampler(tokens)
+
+    assert upsample_fn(downsampled).shape == tokens.shape
