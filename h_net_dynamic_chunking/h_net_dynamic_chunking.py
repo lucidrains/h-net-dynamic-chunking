c00431f535b386baa879f3f0e787e16699879bc7
lucidrains <lucidrains@gmail.com>
2025-07-15 08:05:00 -0700

    handle the moe inspired auxiliary ratio loss

--- h_net_dynamic_chunking/h_net_dynamic_chunking.py
+++ h_net_dynamic_chunking/h_net_dynamic_chunking.py
@@ -3,7 +3,7 @@
 import torch
 from torch import cat
 from torch.nn import Module, Linear, Parameter
-import torch.nn.functional as F
+from torch.nn.functional import cosine_similarity
 
 from einops import repeat, rearrange
 
@@ -22,7 +22,9 @@ class CosineSimRouting(Module):
         self,
         dim,
         dim_queries_keys = None,
-        boundary_threshold = 0.5
+        boundary_threshold = 0.5,
+        target_ratio = 1. / 6.,        # 1/N in eq(10)
+        ratio_loss_weight = 3e-2
     ):
         super().__init__()
         dim_queries_keys = default(dim_queries_keys, dim)
@@ -39,11 +41,18 @@ class CosineSimRouting(Module):
 
         self.boundary_threshold = boundary_threshold
 
+        # ratio aux loss related
+
+        self.target_avg_token_length = 1. / target_ratio
+        self.ratio_loss_weight = ratio_loss_weight
+
+        self.register_buffer('zero', torch.tensor(0.), persistent = False)
+
     def forward(
         self,
         tokens # float[b n d]
     ):
-        batch = tokens.shape[0]
+        batch, length = tokens.shape[:2]
 
         queries, keys = self.to_queries_keys(tokens).chunk(2, dim = -1)
 
@@ -53,7 +62,7 @@ class CosineSimRouting(Module):
 
         # each query looks at the previous key to determine if distance is greater than some threshold for determining a boundary exists (they use 0.5 as threshold)
 
-        cosine_sim  = F.cosine_similarity(queries, keys[:, :-1], dim = -1)
+        cosine_sim  = cosine_similarity(queries, keys[:, :-1], dim = -1)
 
         prob_boundary = (1. - cosine_sim) * 0.5 # cosine sim is -1. to 1., this transforms it to 0. to 1.
 
@@ -63,8 +72,24 @@ class CosineSimRouting(Module):
 
         confidence = torch.where(boundaries, prob_boundary, 1. - prob_boundary)
 
-        # straight through for 1. multiplier on the expanded processed boundary tokens
+        # defaults if not training
 
-        upsampler_output_scale = confidence * (1. - confidence).detach()
+        upsampler_output_scale = 1.
+        aux_ratio_loss = self.zero
 
-        return prob_boundary, boundaries, upsampler_output_scale
+        if self.training or tokens.requires_grad:
+            # straight through for 1. multiplier on the expanded processed boundary tokens
+
+            upsampler_output_scale = confidence * (1. - confidence).detach()
+
+            # auxiliary ratio loss in section 2.3.2, eq (10)
+            # lets follow their notation
+
+            N = self.target_avg_token_length
+
+            F = prob_boundary.sum(dim = -1) / length
+            G = boundaries.sum(dim = -1) / length
+
+            aux_ratio_loss = N / (N - 1) * ((N - 1) * F * G + (1. - F) * (1. - G))
+
+        return prob_boundary, boundaries, upsampler_output_scale, aux_ratio_loss
