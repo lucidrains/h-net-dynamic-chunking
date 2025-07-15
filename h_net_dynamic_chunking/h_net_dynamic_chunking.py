6f93db83c5f3c24f6ac57c8c9393afba0357b6de
lucidrains <lucidrains@gmail.com>
2025-07-15 08:43:50 -0700

    handle the proposed smoothing in eq5

--- h_net_dynamic_chunking/h_net_dynamic_chunking.py
+++ h_net_dynamic_chunking/h_net_dynamic_chunking.py
@@ -1,11 +1,17 @@
 # following section 2.2 of the paper
 
+from collections import namedtuple
+
 import torch
 from torch import cat
+from torch.nested import nested_tensor
 from torch.nn import Module, Linear, Parameter
 from torch.nn.functional import cosine_similarity
 
+import einx
 from einops import repeat, rearrange
+
+from assoc_scan import AssocScan
 
 # helper functions
 
@@ -23,8 +29,9 @@ class CosineSimRouting(Module):
         dim,
         dim_queries_keys = None,
         boundary_threshold = 0.5,
-        target_ratio = 1. / 6.,        # 1/N in eq(10)
-        ratio_loss_weight = 3e-2
+        target_avg_token_length = 6., # N in eq(10)
+        ratio_loss_weight = 3e-2,
+        assoc_scan_use_accelerated = False
     ):
         super().__init__()
         dim_queries_keys = default(dim_queries_keys, dim)
@@ -41,9 +48,13 @@ class CosineSimRouting(Module):
 
         self.boundary_threshold = boundary_threshold
 
+        # smoothing related
+
+        self.smooth_assoc_scan = AssocScan(use_accelerated = assoc_scan_use_accelerated)
+
         # ratio aux loss related
 
-        self.target_avg_token_length = 1. / target_ratio
+        self.target_avg_token_length = target_avg_token_length
         self.ratio_loss_weight = ratio_loss_weight
 
         self.register_buffer('zero', torch.tensor(0.), persistent = False)
@@ -52,7 +63,7 @@ class CosineSimRouting(Module):
         self,
         tokens # float[b n d]
     ):
-        batch, length = tokens.shape[:2]
+        batch, length, device = *tokens.shape[:2], tokens.device
 
         queries, keys = self.to_queries_keys(tokens).chunk(2, dim = -1)
 
@@ -64,13 +75,35 @@ class CosineSimRouting(Module):
 
         cosine_sim  = cosine_similarity(queries, keys[:, :-1], dim = -1)
 
-        prob_boundary = (1. - cosine_sim) * 0.5 # cosine sim is -1. to 1., this transforms it to 0. to 1.
+        probs = (1. - cosine_sim) * 0.5 # cosine sim is -1. to 1., this transforms it to 0. to 1.
 
-        boundaries = prob_boundary > self.boundary_threshold # bool[b n]
+        boundary_mask = probs > self.boundary_threshold # bool[b n]
+
+        # downsampling - they show in their experiments that picking out the boundary tokens works just fine
+
+        lens = boundary_mask.long().sum(dim = -1).tolist()
+
+        boundary_tokens = tokens[boundary_mask] # pick out boundary tokens
+
+        tokens_nt = nested_tensor(boundary_tokens.split(lens), layout = torch.jagged, device = device, requires_grad = True)
+
+        downsampled_tokens = tokens_nt.to_padded_tensor(padding = 0.)
+
+        # smoothing module for improved gradients eq(5)
+
+        probs_nt = nested_tensor(probs[boundary_mask].split(lens), layout = torch.jagged, device = device, requires_grad = True)
+
+        boundary_probs = probs_nt.to_padded_tensor(padding = 0.)
+
+        gates = 1. - boundary_probs
+
+        downsampled_tokens = einx.multiply('b n d, b n', downsampled_tokens, boundary_probs)
+
+        smoothed_downsampled_tokens = self.smooth_assoc_scan(gates, downsampled_tokens)
 
         # for the upsampler
 
-        confidence = torch.where(boundaries, prob_boundary, 1. - prob_boundary)
+        confidence = torch.where(boundary_mask, probs, 1. - probs)
 
         # defaults if not training
 
@@ -87,11 +120,11 @@ class CosineSimRouting(Module):
 
             N = self.target_avg_token_length
 
-            F = prob_boundary.sum(dim = -1) / length
-            G = boundaries.sum(dim = -1) / length
+            F = boundary_mask.float().mean(dim = -1)
+            G = probs.mean(dim = -1)
 
             aux_ratio_loss = N / (N - 1) * ((N - 1) * F * G + (1. - F) * (1. - G))
 
             aux_loss = aux_ratio_loss.mean() * self.ratio_loss_weight
 
-        return prob_boundary, boundaries, upsampler_output_scale, aux_loss
+        return smoothed_downsampled_tokens, probs, boundary_mask, upsampler_output_scale, aux_loss
